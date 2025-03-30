afc16927bb50122d7b5ef18694af2cd905fbc61f
WoojinKim1225 <kwj69641@gmail.com>
2025-03-30 16:08:46 +0900

    Feat: Added Name

--- cweek04a/src/main/java/com/example/cweek04a/uicomponents/MainScreen.kt
+++ cweek04a/src/main/java/com/example/cweek04a/uicomponents/MainScreen.kt
@@ -3,6 +3,7 @@ package com.example.cweek04a.uicomponents
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -12,6 +13,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.example.cweek04a.model.TodoItemFactory
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.unit.sp
 import com.example.cweek04a.model.Item
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
@@ -27,6 +29,9 @@ fun MainScreen(modifier: Modifier = Modifier) {
     var s_showPending by remember { mutableStateOf(false) }
     Column(Modifier.padding(10.dp, 10.dp)) {
         TodoListTitle()
+        Text(
+            text = "202110728 김우진",
+            fontSize = 18.sp)
         TodoShowPendingToggle(
             modifier = Modifier,
             isChecked = s_showPending) {
